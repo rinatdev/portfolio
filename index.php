bf660c40189aa803238c20648668dbc9c83fd151
rinatdev <92872938+rinatdev@users.noreply.github.com>
2021-11-19 12:53:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,20 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-
     <title>Rinat</title>
 </head>
 
 <body>
     <div class="d-flex bd-highlight mb-2 bc">
-        <a class="navbar-brand me-auto p-2 bd-highlight text-light" href="#">
+        <a class="navbar-brand me-auto p-2 bd-highlight logo-hover" href="#">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlFn9cvjr_wlX0t0wLmyQZp2vNBR27N0saWg&usqp=CAU" alt="" width="30" height="24" class="d-inline-block align-text-top"> Rinat
         </a>
         <a class="p-2 bd-highlight" href="#">
             <img src="https://futuredreamprojects.com/logo/dd.jpg" alt="" width="26" height="26" class="d-inline-block align-text-top">
-            <a class="nav-link mt-1 ps-0 text-light" href="#">Sign in</a>
+            <a class="nav-link mt-1 ps-0 hover" href="avtorization.html">Sign in</a>
         </a>
     </div>
+
+    <div class="d-flex">
+        <div class="d-block m-5">
+            <img class="ava" src="ava.jpeg" alt="">
+        </div>
+        <div class="card border-0 align-self-center" style="margin-left: 11rem; background-color: black; color: #fff;">
+            <div class="card-body border-light mb-3" style="max-width: 36rem;">
+                <h5 class="card-title">HI THERE !</h5>
+                <h1 class="card-title">I'M <a style="color: rgb(255, 196, 0);">WEB DEVELOPER</a></h1>
+                <p class="card-text">I’m a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
+                <a href="#" class="btn btn-warning">MORE ABOUT ME</a>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
 
